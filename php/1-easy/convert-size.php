3da2be9cc2250b7fa3235f9eddef9db3c9f7b4d7
EstelleJ <contact@estellejacquet.fr>
2022-12-30 19:20:35 +0100

    PHP - convertSize function refactoring

---
+++ php/1-easy/convert-size.php
@@ -0,0 +1,25 @@
+<?php
+
+$unitsArray = [
+		'B',
+		'KB',
+		'MB',
+		'GB',
+		'TB',
+		'PB',
+		'EB',
+		'ZB',
+		'YB',
+		'HB',
+];
+
+function convertSize($fileSize, $precision = 2, $i = 0): string {
+	global $unitsArray;
+
+	if($fileSize > 1024) {
+		$fileSize = $fileSize / 1024;
+		return convertSize($fileSize, $precision, ++$i);
+	} else {
+		return round($fileSize, $precision) . ' ' . $unitsArray[$i];
+	}
+}
